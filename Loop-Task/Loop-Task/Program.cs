baafe09fbf7399ab6b0aa3f125133c97a555d9cd
AniMkrtchyan2 <51071435+AniMkrtchyan2@users.noreply.github.com>
2019-06-23 20:04:28 +0400

    Initial commit

---
+++ Loop-Task/Loop-Task/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Task
+{
+    class Program
+    { //Task17 find n number smallest digits
+
+        /* static void Main()
+         {
+             //int operand = 23654872;
+             int operand=32;
+             //int result = SmallDigits( out operand);
+             // int result = SumDigits(out operand);
+             // int result = MultiDigits(ref operand);
+             // int result = SubiDigits(out operand);
+             int result = DivNumber(ref operand);
+             Console.WriteLine(result);
+
+         }
+         ///// <summary>
+         ///// /Smallest digits
+         ///// </summary>
+         ///// <param name="number" out int></param>
+         ///// <returns></returns>
+         //    static int  SmallDigits(out int number)
+         //    { 
+
+         //     number = 2469831;
+         //    int a = 9;
+         //    int Digit;
+         //    while (number != 0)
+         //    {
+         //        Digit = number % 10;
+
+         //        if (Digit % 2 == 1 && (Digit - a) < 0)
+         //        {
+         //            a = Digit;
+         //        }
+         //        number /= 10;
+         //    }
+         //        return a;
+         //    }
+
+         //Task 18 Compute number digits >4 && odd digits sum.
+         //static int SumDigits(out int number)
+         //{
+         //    int Sum = 0;
+         //    int Digit;
+         //    number = 2365487;
+         //    while (number != 0)
+         //    {
+         //        Digit = number % 10;
+
+         //        if (Digit % 2 == 1 && Digit > 4)
+         //        {
+         //            Sum += Digit;
+         //        }
+         //        number /= 10;
+         //    }
+         //    return Sum;
+         //}
+         //Task 19 Compute number digits <7 && even digits multi.
+
+         //static int MultiDigits(ref int number)
+         //{
+         //    int multi = 1;
+         //    int Digit;
+         //    while (number != 0)
+         //    {
+         //        Digit = number % 10;
+
+         //        if (Digit % 2 == 0 && Digit < 7)
+         //        {
+         //            multi *= Digit;
+         //        }
+         //        number /= 10;
+         //    }
+         //    return multi;
+
+
+         // Task 20 number  smallest and big digits Sub and ^2
+         //static int SubiDigits(out int number)
+         //{
+         //    number = 256347;
+         //    int a = 9;
+         //    int b = 0;
+         //    int Digit;
+         //    while (number != 0)
+         //    {
+         //        Digit = number % 10;
+
+         //        if ((Digit - a) < 0)
+         //        {
+         //            a = Digit;
+         //        }
+         //        if (Digit > b)
+         //        {
+         //            b=Digit;
+         //        }
+         //        number /= 10;
+         //    }
+         //    int sub = a - b;
+         //    int c = sub * sub;
+         //    return c;
+         //}
+         //Task 21 print number all divisor*/
+
+        //  
+
+        static void Main(string[] args)
+        //{ int i;
+        //    int number = Convert.ToInt32(Console.ReadLine());
+
+        //    for (i = 2; i <= number / 2; i++)
+        //    { 
+        //        if (number % i == 0)
+        //        {
+
+        //           Console.WriteLine(i);
+        //        }
+
+        //    }
+        {
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //double a;
+            //int j = 1;
+            //if (number != 1)
+            //    for (int i = 2; i <= number / 2; i++)
+            //    {
+            //        if (number % i == 0)
+            //        {
+            //            sum += i;
+            //            j++;
+            //            // a = sum / j;
+            //            // Console.WriteLine(j);  }
+            //        a = sum / j;
+            //        Console.WriteLine(a);
+            //    }
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //if (number % 3 == 0)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //    Console.WriteLine("No");
+            //Console.ReadKey();
+            // Task 25 +
+            //   Task26+
+            //Task 27
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int digit;
+            //while (number != 0)
+            //{
+            //    digit = number % 10;
+            //    number /= 10;
+            //    Console.WriteLine(digit);
+            //    Console.WriteLine(0);
+            //}
+            //Task 28, 29 +
+            // Task 30
+        //    int number = Convert.ToInt32(Console.ReadLine());
+        //    int digit1, digit12;
+        //    int digit2, digit34;
+        //    int digit3, sum;
+        //    int digit4, sub;
+
+        //    if (number >= 1000 && number < 10000)
+        //    {
+                
+        //        {
+        //            digit34 = number % 100;
+
+        //            digit1 = number % 10;
+        //            number /= 10;
+        //            digit2 = number % 10;
+        //            digit12 = number / 10;
+        //            number /= 10;
+        //            digit3 = number % 10;
+        //            number /= 10;
+        //            digit4 = number % 10;
+        //        }
+
+        //        if (digit1 != digit2 && digit2 != digit3 && digit3 != digit4
+        //            && digit1 != digit3 && digit2 != digit4 && digit4 != digit1)
+        //        {
+        //            sum = digit1 + digit2 + digit3 + digit4;
+        //            sub = digit12 - digit34;
+        //            if (sub == sum)
+        //            {
+        //                Console.WriteLine(sum); }
+        //            else
+        //                Console.WriteLine(sub);
+        //        }
+        //    }
+           
+
+        //}
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+    }
+
+
+}
+
